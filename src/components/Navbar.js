1245946f014cf96ecfe232bd7cd155d42237286d
Catherine A. Samuel <casamuel2u@gmail.com>
2022-05-26 19:00:52 -0400

    change the action on the Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,19 +2,26 @@ import {BsCart } from 'react-icons/bs';
 import {GiHamburgerMenu} from "react-icons/gi"
 import "../styles/navbar.css";
 import { useNavigate } from 'react-router-dom';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect} from 'react';
 
 export const Navbar =({counter}) => {
-
+  
   const navigate = useNavigate();
   const [toggleMenu, setToggleMenu] = useState(false);
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  
+    const scrollToShop= () => {
+        navigate("/");
+        window.scrollTo({top:200, left:100, behavior:"smooth"});
+        setToggleMenu(!toggleMenu);
 
-
-
-    const toggleNav = () => {
-      setToggleMenu(!toggleMenu)
-    }    
+  }
+    const scrollToProduct = () =>{
+        navigate("/");
+        window.scrollTo(0, window.innerHeight);
+        setToggleMenu(!toggleMenu);
+    }
+  
     
     useEffect(() => {
       const changeWidth = () => {
@@ -26,18 +33,23 @@ export const Navbar =({counter}) => {
     }, [screenWidth])    
   
     return (
-      <nav className="navbar">
+      
+      <div className="navbar">
+        <div>
+            <GiHamburgerMenu style={{width:"50px", height:"50px", textAlign:"right"}} classname="menu-btn" onClick={()=>setToggleMenu(!toggleMenu)}/> 
+        </div>
         {(toggleMenu || screenWidth > 600) && (
           <ul className = "menu-container">
-            <li className="items" onClick={() => navigate("/")}>Home</li>
-            <li className="items" onClick={() => navigate("/")}>Product</li>
-            <li className="items" onClick={() => navigate("/cart")}>Cart</li>
-            <li className="items" onClick={() => navigate("/cart")}><BsCart/></li>
-            <li className="items" style={{border: "0.1rem solid lightblue", width:"100px", height:"100px",margin_right:"40rem"}} onClick={() => navigate("/cart")}>{counter}</li>
+              <li className="items" onClick={scrollToShop} >Home</li>
+              <li className="items" onClick={scrollToProduct}>Product</li>
+              <li className="items" onClick={() => navigate("/cart")}>Cart</li>
+              <li className="items" onClick={() => navigate("/cart")}><BsCart/></li>
+              <li className="items" style={{border: "0.1rem solid lightblue", width:"80px", 
+            height:"20px", margin_right:"40rem"}} onClick={() => navigate("/cart")}>{counter}</li>
           </ul>
         )}
-      <button onClick={toggleNav}><GiHamburgerMenu className="menu-btn"/></button>     
-    </nav>
+        
+    </div>
 )
 };
 
