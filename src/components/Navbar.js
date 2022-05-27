b8dbf6d5f565e07e0159bef38e587acce39214ba
Catherine A. Samuel <casamuel2u@gmail.com>
2022-05-27 13:06:37 -0400

    correction to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,9 +35,11 @@ export const Navbar =({counter}) => {
     return (
       
       <div className="navbar">
+      
         <div>
-            <GiHamburgerMenu style={{width:"50px", height:"50px", textAlign:"right"}} classname="menu-btn" onClick={()=>setToggleMenu(!toggleMenu)}/> 
+            <GiHamburgerMenu style={{width:"50px", height:"50px", textAlign:"right"}} className="menu-btn" onClick={()=>setToggleMenu(!toggleMenu)}/> 
         </div>
+        
         {(toggleMenu || screenWidth > 600) && (
           <ul className = "menu-container">
               <li className="items" onClick={scrollToShop} >Home</li>
@@ -45,7 +47,7 @@ export const Navbar =({counter}) => {
               <li className="items" onClick={() => navigate("/cart")}>Cart</li>
               <li className="items" onClick={() => navigate("/cart")}><BsCart/></li>
               <li className="items" style={{border: "0.1rem solid lightblue", width:"80px", 
-            height:"20px", margin_right:"40rem"}} onClick={() => navigate("/cart")}>{counter}</li>
+            height:"30px", margin_right:"40rem"}} onClick={() => navigate("/cart")}>{counter}</li>
           </ul>
         )}
         
